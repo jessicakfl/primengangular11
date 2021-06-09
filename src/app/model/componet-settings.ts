6ca5b1e6030ab822cb7dc99de980863d45780836
jessica bai <jessicakfl@Jessicas-MacBook-Pro.local>
2021-06-09 16:37:02 -0700

    info

---
+++ src/app/model/componet-settings.ts
@@ -0,0 +1,14 @@
+export class ComponetSettings {
+    componentName: string;
+    nw: number;
+    nh: number;
+    nt: number;
+    nl: number;
+    id:number
+    // public getComponetName(){
+    //     return this.componentName;
+    // }
+    // public setComponetName(componentName: string){
+    //     this.componentName = componentName;
+    // }
+}
