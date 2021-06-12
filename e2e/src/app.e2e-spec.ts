7cae6babacc4f06a2a06836870f3af94bf945090
jessica bai <jessicakfl@Jessicas-MacBook-Pro.local>
2021-06-12 11:12:49 -0700

    test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-resizable-draggable app is running!');
+    expect(page.getTitleText()).toEqual('primengangular11 app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-resizable-draggable'`, () => {
+  it(`should have as title 'primengangular11'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-resizable-draggable');
+    expect(app.title).toEqual('primengangular11');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-resizable-draggable app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('primengangular11 app is running!');
   });
 });
