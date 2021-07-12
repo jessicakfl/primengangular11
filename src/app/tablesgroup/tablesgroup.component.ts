8fdb1f8c05aee86eb80e8451cad524b58997a163
jessica bai <jessicakfl@Jessicas-MacBook-Pro.local>
2021-07-12 09:58:32 -0700

    edit

--- src/app/tablesgroup/tablesgroup.component.ts
+++ src/app/tablesgroup/tablesgroup.component.ts
@@ -1,29 +1,44 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
-
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+];
+const ELEMENT_DATA2: PeriodicElement[] = [
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: '123', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Litllium', weight: 9.0122, symbol: 'Be' }
+];
 @Component({
   selector: 'app-tablesgroup',
   templateUrl: './tablesgroup.component.html',
   styleUrls: ['./tablesgroup.component.css']
 })
 export class TablesgroupComponent implements OnInit {
-  @ViewChild('dragHandleTmpl') dragHandleTmpl; 
-  constructor() { }
-
   ngOnInit(): void {
   }
+  title = 'drag tables';
+  dataSource = ELEMENT_DATA;
+  dataSource2 = [];
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      moveItemInArray(event.container.data,
+        event.previousIndex,
+        event.currentIndex);
     } else {
       transferArrayItem(event.previousContainer.data,
-                        event.container.data,
-                        event.previousIndex,
-                        event.currentIndex);
+        event.container.data,
+        event.previousIndex, event.currentIndex);
     }
   }
-
 
 }
