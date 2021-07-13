a114fc04332ee342052f2d8753638067c0870c39
jessica bai <jessicakfl@Jessicas-MacBook-Pro.local>
2021-07-12 18:01:22 -0700

    edit

--- src/app/tablesgroup/tablesgroup.component.ts
+++ src/app/tablesgroup/tablesgroup.component.ts
@@ -14,7 +14,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 ];
 const ELEMENT_DATA2: PeriodicElement[] = [
   { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: '123', weight: 4.0026, symbol: 'He' },
+  // { position: 2, name: '123', weight: 4.0026, symbol: 'He' },
   // { position: 3, name: 'Litllium', weight: 9.0122, symbol: 'Be' }
 ];
 const ELEMENT_DATA3: PeriodicElement[] = [
@@ -39,10 +39,10 @@ export class TablesgroupComponent implements OnInit {
   ifimagepaging: boolean;
   title = 'drag tables';
   dataSource = ELEMENT_DATA;
-  dataSource2 = [];
+  dataSource2 = ELEMENT_DATA2;
   configList: any[];
   dataSource3 = ELEMENT_DATA3;
-  dataSource4 = [];
+  dataSource4 = ELEMENT_DATA4;
   displayedColumns: string[] = ['position', 'name'];
 
   drop(event: CdkDragDrop<string[]>) {
