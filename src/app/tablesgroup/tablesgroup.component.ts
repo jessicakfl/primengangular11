53cea44f60862f12d90a71ffa0dcec00701cfca4
jessica bai <jessicakfl@Jessicas-MacBook-Pro.local>
2021-07-13 14:59:05 -0700

    bug fixes

--- src/app/tablesgroup/tablesgroup.component.ts
+++ src/app/tablesgroup/tablesgroup.component.ts
@@ -13,7 +13,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   // { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
 ];
 const ELEMENT_DATA2: PeriodicElement[] = [
-  { position: 2, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  // { position: 2, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
   // { position: 2, name: '123', weight: 4.0026, symbol: 'He' },
   // { position: 3, name: 'Litllium', weight: 9.0122, symbol: 'Be' }
 ];
@@ -22,7 +22,7 @@ const ELEMENT_DATA3: PeriodicElement[] = [
   // { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
 ];
 const ELEMENT_DATA4: PeriodicElement[] = [
-  { position: 4, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  // { position: 4, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
   // { position: 2, name: '123', weight: 4.0026, symbol: 'He' },
   // { position: 3, name: 'Litllium', weight: 9.0122, symbol: 'Be' }
 ];
@@ -42,7 +42,7 @@ export class TablesgroupComponent implements OnInit {
   dataSource2 = ELEMENT_DATA2;
   configList: any[];
   dataSource3 = ELEMENT_DATA3;
-  dataSource4 = [];
+  dataSource4 = ELEMENT_DATA4;
   displayedColumns: string[] = ['position', 'name'];
   widthImage: number = 500;
   heightImage: number = 135;
