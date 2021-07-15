6ad4c8f040ed33f93ab53ced99b2ad691ab989a5
jessica bai <jessicakfl@Jessicas-MacBook-Pro.local>
2021-07-14 17:18:17 -0700

    display

--- src/app/tablesgroup/tablesgroup.component.ts
+++ src/app/tablesgroup/tablesgroup.component.ts
@@ -57,7 +57,7 @@ export class TablesgroupComponent implements OnInit {
   displayedColumns: string[] = ['position', 'name'];
   widthImage: number = 500;
   heightImage: number = 135;
-  topImage: number = -80;
+  topImage: number = -50;
   leftImage: number = 0;
 
   drop(event: CdkDragDrop<string[]>) {
