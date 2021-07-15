4db832873b91047cd49fb8032096f8e9f54dfacb
jessica bai <jessicakfl@Jessicas-MacBook-Pro.local>
2021-07-15 10:14:28 -0700

    update

--- src/app/tablesgroup/tablesgroup.component.ts
+++ src/app/tablesgroup/tablesgroup.component.ts
@@ -13,27 +13,21 @@ const ELEMENT_DATA: PeriodicElement[] = [
   // { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
 ];
 const ELEMENT_DATA2: PeriodicElement[] = [
-  // { position: 2, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  // { position: 2, name: '123', weight: 4.0026, symbol: 'He' },
-  // { position: 3, name: 'Litllium', weight: 9.0122, symbol: 'Be' }
 ];
 const ELEMENT_DATA3: PeriodicElement[] = [
   { position: 2, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
   // { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
 ];
 const ELEMENT_DATA4: PeriodicElement[] = [
-  // { position: 4, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  // { position: 2, name: '123', weight: 4.0026, symbol: 'He' },
-  // { position: 3, name: 'Litllium', weight: 9.0122, symbol: 'Be' }
 ];
 const ELEMENT_DATA5: PeriodicElement[] = [
   { position: 3, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  // { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
 ];
 const ELEMENT_DATA6: PeriodicElement[] = [
-  // { position: 2, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  // { position: 2, name: '123', weight: 4.0026, symbol: 'He' },
-  // { position: 3, name: 'Litllium', weight: 9.0122, symbol: 'Be' }
+];
+const ELEMENT_DATA7: PeriodicElement[] = [
+];
+const ELEMENT_DATA8: PeriodicElement[] = [
 ];
 @Component({
   selector: 'app-tablesgroup',
@@ -54,6 +48,8 @@ export class TablesgroupComponent implements OnInit {
   dataSource4 = ELEMENT_DATA4;
   dataSource5 = ELEMENT_DATA5;
   dataSource6 = ELEMENT_DATA6;
+  dataSource7 = ELEMENT_DATA7;
+  dataSource8 = ELEMENT_DATA8;
   displayedColumns: string[] = ['position', 'name'];
   widthImage: number = 516;
   heightImage: number = 160;
